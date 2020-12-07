7fea4bf9a3fed0f1b1994e2a917460e80bae48ee
rongbang <rongbang.ye@gmail.com>
2020-12-06 22:06:55 -0800

    Deploying

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
